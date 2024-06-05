91fda0fd4f05ab32e8116d4cb4c3d80ecf58d4a4
Ubuntu <ubuntu@ip-172-31-37-95.ap-northeast-2.compute.internal>
2024-06-05 11:21:57 +0000

    fe be 머지

--- main.js
+++ main.js
@@ -3,9 +3,28 @@ const app=express() // express 실행
 
 app.listen(3000, ()=>{ // 서버 생성, 포트번호:3000
     console.log('서버 시작');
+    
+    //회원가입
+    app.get('/register',(req,res)=>{
+        console.log('가입 페이지에 대한 요청');
+        res.sendFile(__dirname+'/pages/register.html');
+    })
 
+    //로그인
+    app.get('/signin',(req,res)=>{
+        console.log(' 로그인 페이지에 대한 요청');
+        res.sendFile(__dirname+'/pages/signin.html');
+    })
+
+    //메인 페이지
     app.get('/',(req,res)=>{
-        console.log('루트에 대한 요청');
-        res.send('루트페이지에 대한 요청');
-    }) 
+        console.log('메인페이지에 대한 요청');
+        res.sendFile(__dirname+'/pages/main.html');
+    })
+
+    //사용자 페이지
+    app.get('/userinfo',(req,res)=>{
+        console.log('사용자 페이지에 대한 요청');
+        res.sendFile(__dirname+'/pages/userinfo.html');
+    })
 }) 
