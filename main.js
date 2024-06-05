637dda459ec9798f1b25041f3915b530df1b831b
Wooki <hyunook02@naver.com>
2024-06-05 20:12:32 +0900

    modified 회원가입, 로그인

--- main.js
+++ main.js
@@ -11,25 +11,25 @@ app.listen(3000, ()=>{ // 서버 생성, 포트번호:3000
 
     //회원가입
     app.get('/register',(req,res)=>{
-        console.log('루트에 대한 요청');
-        res.send('루트페이지에 대한 요청');
+        console.log('가입 페이지에 대한 요청');
+        res.sendFile(__dirname+'/pages/register.html');
     })
 
     //로그인
     app.get('/signin',(req,res)=>{
-        console.log('루트에 대한 요청');
-        res.send('루트페이지에 대한 요청');
+        console.log(' 로그인 페이지에 대한 요청');
+        res.sendFile(__dirname+'/pages/register.html');
     })
 
     //메인 페이지
     app.get('/',(req,res)=>{
-        console.log('루트에 대한 요청');
+        console.log('메인페이지에 대한 요청');
         res.sendFile(__dirname+'/pages/main.html');
     })
 
     //사용자 페이지
     app.get('/userinfo',(req,res)=>{
-        console.log('루트에 대한 요청');
+        console.log('사용자 페이지에 대한 요청');
         res.sendFile(__dirname+'/pages/userinfo.html');
     })
 }) 
