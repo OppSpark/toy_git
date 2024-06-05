3626af63f35f96760da69e6e86cbf8a586f6b966
Wooki <hyunook02@naver.com>
2024-06-05 20:03:18 +0900

    feat: 회원가입 로그인 폼 추가

--- main.js
+++ main.js
@@ -10,9 +10,17 @@ app.listen(3000, ()=>{ // 서버 생성, 포트번호:3000
     })
 
     //회원가입
+    app.get('/register',(req,res)=>{
+        console.log('루트에 대한 요청');
+        res.send('루트페이지에 대한 요청');
+    })
 
     //로그인
-    
+    app.get('/signin',(req,res)=>{
+        console.log('루트에 대한 요청');
+        res.send('루트페이지에 대한 요청');
+    })
+
     //메인 페이지
 
     //사용자 페이지
